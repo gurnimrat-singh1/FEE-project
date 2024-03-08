0772cd2559d52878305494e2b853d39693e855a6
gurmehakk20 <125135241+gurmehakk20@users.noreply.github.com>
2024-03-08 15:59:22 +0530

    Update gold.js

--- gold.js
+++ gold.js
@@ -1,11 +1,2 @@
-// You can add interactivity or additional functionality to the navbar using JavaScript.
-// Here's an example of toggling a mobile navbar:
-
-// const mobileNav = document.querySelector('.mobile-nav');
-// const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
-
-// mobileNavToggle.addEventListener('click', () => {
-// 	mobileNav.classList.toggle('open');
-// });
-
+// API
 fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo").then((response)=>response.json()).then((data)=>console.log(data));
