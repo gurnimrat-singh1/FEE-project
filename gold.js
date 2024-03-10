e373e028130536d61e8bc7989717f0815a84583d
gurnimrat-singh1 <128783108+gurnimrat-singh1@users.noreply.github.com>
2024-03-10 18:55:22 +0530

    Updated gold.js

--- gold.js
+++ gold.js
@@ -9,3 +9,33 @@
 // });
 
 fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo").then((response)=>response.json()).then((data)=>console.log(data));
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        const slides = document.getElementsByClassName("mySlides");
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+    }
+	
