98ed8d131f1c466a02b7738c94a9412a9e4d8048
gurmehakk20 <125135241+gurmehakk20@users.noreply.github.com>
2024-03-10 14:22:41 +0530

    Delete gold.js

--- gold.js
+++
@@ -1,2 +0,0 @@
-// API
-fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo").then((response)=>response.json()).then((data)=>console.log(data));
